c5bef2b480f158e34b52907a7d18ae6fee6c4cd8
OleksiySolomakha <alekseysomik@gmail.com>
2025-12-04 20:28:48 +0200

    create some phrese

--- JSPractice1/main.js
+++ JSPractice1/main.js
@@ -1,3 +1,6 @@
 import chalk from "chalk";
 
 console.log(chalk.green("Hello World"));
+console.log(chalk.redBright("Nice collor"));
+console.log(chalk.blueBright("Good for Now"));
+console.log(chalk.yellowBright("I think"));
