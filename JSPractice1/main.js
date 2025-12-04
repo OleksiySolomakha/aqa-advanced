125f2637130b7d89b375cd3cb702f0d5eb5510b3
OleksiySolomakha <alekseysomik@gmail.com>
2025-12-04 21:37:55 +0200

    lesson3 -added chunk

--- JSPractice1/main.js
+++
@@ -1,6 +0,0 @@
-import chalk from "chalk";
-
-console.log(chalk.green("Hello World"));
-console.log(chalk.redBright("Nice collor"));
-console.log(chalk.blueBright("Good for Now"));
-console.log(chalk.yellowBright("I think"));
---
+++ nodeBasics/lesson3/index.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+console.log(chalk.green("Hello World"));
+console.log(chalk.redBright("Nice collor"));
+console.log(chalk.yellowBright("I think"));
+
+let age = 20;
+
+const firstName = "John";
+
+console.log(chalk.bgCyanBright(firstName, age));
