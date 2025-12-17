f5d142d043dc89b6fd443279e98d503562394c70
OleksiySolomakha <alekseysomik@gmail.com>
2025-12-17 21:14:43 +0200

    practicejs_lesson6: - task1.js added functions for calculation squre in different cases - task2.js added checking age function - task3.js added checking order

---
+++ jsBasics/lesson6/task2.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+function agesm(age){
+    if(age<18) {
+        console.log(chalk.bgYellowBright('You are too young, youre age is: ', age))
+    } else if(age) {
+        console.log(chalk.bgGreenBright(`Welcome to the club buddy, ${age} is enought`))
+    }
+}
+
+agesm(15);
+agesm(25);
---
+++ jsBasics/lesson6/task3.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+
+function checkOrder(available, ordered) {
+    if(available < ordered) {
+        console.log(chalk.bgCyanBright('Your order is too large, we don’t have enough goods'))
+    } else if(ordered == 0) {
+        console.log(chalk.bgMagentaBright('Your order is empty'))
+    } else {
+        console.log(chalk.bgWhite('Your order is accepted'))
+    }
+}
+
+checkOrder(15,33);
+checkOrder(1,0);
+checkOrder(3,2);
