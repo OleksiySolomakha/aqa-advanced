f906348853674c45ad542a5f49216fbf874eb2a6
OleksiySolomakha <alekseysomik@gmail.com>
2025-12-27 22:43:01 +0200

    practicejs_lesson9 - shows some object - show different keys from object - used spread operator for create new object from two others - tried add new key and delete old key in object - practice with for ...of metod for masive of objects

---
+++ jsBasics/lesson9/task1.js
@@ -0,0 +1,8 @@
+const student = {
+    firstName: 'Borys',
+    lastName: 'Britva',
+    age: 18,
+    courses: ["Математика", "Історія", "Програмування"]
+};
+
+console.log('Object student is:', student);
