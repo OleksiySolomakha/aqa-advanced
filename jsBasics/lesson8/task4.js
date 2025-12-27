8092c217dd2295a0814e0d0f5e28ce7268b81c42
OleksiySolomakha <alekseysomik@gmail.com>
2025-12-27 21:29:49 +0200

    practicejs_lesson8
    - count positive, negative and zero numbers in mas
    - added multiple mas function
    - added count sum of mas
    - added even num filter function
    - added concat some mas
    - added special sort function

---
+++ jsBasics/lesson8/task4.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function filterEvenNums(massive) {
+    return massive.filter((number) => number % 2 === 0);
+}
+
+let evenFromMas = filterEvenNums(nums);
+
+console.log(chalk.yellow('nums =', nums));
+console.log(chalk.green('evenFromMas =', evenFromMas));
---
+++ jsBasics/lesson8/task5.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+
+const firstArray = [2, 4, 'green', 'Subaru'];
+const secondArray = [1, 9, 'red', 'BMW'];
+
+function addMassives(first, second) {
+    return first.concat(second);
+}
+
+let concatMas = addMassives(firstArray, secondArray);
+
+console.log(chalk.bgYellow('firstArray =', firstArray));
+console.log(chalk.bgBlue('secondArray =', secondArray));
+console.log(chalk.bgGreen('concatMas =', concatMas));
