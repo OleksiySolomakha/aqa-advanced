929c26fea2b2468ea31e27100a3b9bb4b5c00add
OleksiySolomakha <alekseysomik@gmail.com>
2026-01-22 01:22:57 +0200

    practicejs_lesson11 - added small delay function - created a few function with then, catch to get todo and user information from the link - used async/await for create function to get some information from the link - create class whitch get information from the link

---
+++ jsBasics/lesson11/getSomeLinkData.js
@@ -0,0 +1,26 @@
+class GetSomeLinkData {
+    constructor(link){
+        this.link = link;
+    } 
+
+    getLinkData() {
+        return fetch(this.link)
+        .then(
+            response => {
+                if(!response.ok) {
+                    throw new Error('Something goes wrong')
+                } else {
+                    return response.json()
+                }
+            }
+        ).then(data => {
+            console.log('the data from link is: ', data)
+            return data;
+        }).catch(error => {
+            console.error('It\'s probably:', error.message);
+        })
+      }
+}
+
+export  {GetSomeLinkData}
+
---
+++ jsBasics/lesson11/task3.js
@@ -0,0 +1,33 @@
+import {GetSomeLinkData} from './getSomeLinkData.js';
+
+let toDoValidLnk = 'https://jsonplaceholder.typicode.com/todos/1';
+let userValidLink = 'https://jsonplaceholder.typicode.com/users/1'
+
+//Use this variable to get an error
+let invalidLink = 'https://jsonplaceholder.typicode.com';
+
+async function getLinkData(url) {
+    const response = await fetch(url);
+    try {
+      const data = await response.json();
+      console.log('GET Response:', data);
+    } catch (error) {
+      console.error('It\'s probebly:', error);
+    }
+  }
+
+//   getLinkData(userValidLink);
+//   getLinkData(invalidLink);
+//   getLinkData(toDoValidLnk)
+
+// Task 4
+let myToDo = new GetSomeLinkData(toDoValidLnk);
+let myUser = new GetSomeLinkData(userValidLink);
+let myError = new GetSomeLinkData(invalidLink)
+
+// myToDo.getLinkData();
+myUser.getLinkData();
+// myError.getLinkData();
+
+
+  
